87009fbeec279883231351d510a1942d230f7806
liugaowei999 <187346454@qq.com>
2017-12-25 16:50:25 +0800

    first create smart framework

---
+++ smart-framework/src/main/java/com/smart4j/framework/ConfigConstant.java
@@ -0,0 +1,21 @@
+package com.smart4j.framework;
+
+/**
+ * 相关的配置项常量
+ * 
+ * @author liugaowei
+ *
+ */
+public interface ConfigConstant {
+
+	String CONFIG_FILE = "smart.properties";
+
+	String JDBC_DRIVER = "smart.framework.jdbc.driver";
+	String JDBC_URL = "smart.framework.jdbc.url";
+	String JDBC_USERNAME = "smart.framework.jdbc.username";
+	String JDBC_PASSWORD = "smart.framework.jdbc.password";
+
+	String APP_BASE_PACKAGE = "smart.framework.app.base_package";
+	String APP_JSP_PATH = "smart.framework.app.jsp_path";
+	String APP_ASSERT_PATH = "smart.framework.app.assert_path";
+}
---
+++ smart-framework/src/main/java/com/smart4j/utils/PropsUtil.java
@@ -0,0 +1,10 @@
+package com.smart4j.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PropsUtil {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PropsUtil.class);
+
+}
